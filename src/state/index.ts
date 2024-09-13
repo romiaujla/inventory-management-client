a0033f8874cb47682865be6cec15cc9b07f8f6f9
Raman Aujla <raujla@developrise.com>
2024-09-13 00:16:33 -0400

    IMC-004 - created global reducers for setting sidebar collapsed and dark mode toggle

--- src/state/index.ts
+++ src/state/index.ts
@@ -9,3 +9,20 @@ const initialState: InitialStateTypes = {
   isSidebarCollapsed: false,
   isDarkMode: false,
 };
+
+export const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers: {
+    setIsSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
+      state.isSidebarCollapsed = action.payload;
+    },
+    setIsDarkMode: (state, action: PayloadAction<boolean>) => {
+      state.isDarkMode = action.payload;
+    },
+  },
+});
+
+export const { setIsSidebarCollapsed, setIsDarkMode } = globalSlice.actions;
+
+export default globalSlice.reducer;
