81349c18635c1a52648cbcdbbee11e73c0df6777
Raman Aujla <raujla@developrise.com>
2024-09-13 18:21:54 -0400

    IMC-007 - Updated Dark vs Light mode icon

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/state/index";
@@ -51,8 +51,12 @@ const NavBar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}}>
-              <Sun className="cursor-pointer text-gray-500" size={24} />
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
             </button>
           </div>
           <div className="relative">
