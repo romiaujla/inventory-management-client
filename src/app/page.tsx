1a942573c07e3f1538f7c8fd1771b0a618d059ea
Raman Aujla <raujla@developrise.com>
2024-09-12 23:48:40 -0400

    IMC-002 - created a Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import Dashboard from "@/app/dashboard/page";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      Hello
-    </main>
-  );
+  return <Dashboard />;
 }
